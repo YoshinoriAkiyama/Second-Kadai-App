ff68a61feba44d8324199cbc80817f8a560c312d
yoshinori.akiyama <akiyama.dime@gmail.com>
2020-10-11 09:50:55 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Yoshinori Akiyama on 2020/10/11.
+//  Copyright © 2020 Yoshinori Akiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
